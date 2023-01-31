22895a3b0007226c7f7cc46bc9303c1b522fc0fa
eosull <osullivaneoin442@gmail.com>
2023-01-31 16:34:25 +0000

    feat: verify user name is not too long

--- run.py
+++ run.py
@@ -4,9 +4,25 @@
 
 def ask_player_name():
     print("Hello! Welcome to Battleship")
-    name = input("Enter your name here:\n")
 
-    print(f"Thank you {name}, let's get ready to play!")
+    while True:
+        name = input("Enter your name here:\n")
+
+        if validate_input("name", name):
+            print(f"Thank you {name}, let's get ready to play!")
+            break
+
+
+def validate_input(input_type, data):
+    if input_type == "name":
+        try:
+            if len(data) > 15:
+                raise ValueError("More than 15 characters")
+        except ValueError as e:
+            print(f"Invalid name: {e}, please try again.\n")
+            return False
+
+        return True
 
 
 ask_player_name()
