cb0070ba954bbf7eadb553db70c36d8a312be13d
eosull <osullivaneoin442@gmail.com>
2023-01-31 15:57:20 +0000

    feat: accept and display user name input

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def ask_player_name():
+    print("Hello! Welcome to Battleship")
+    name = input("Enter your name here:\n")
+
+    print(f"Thank you {name}, let's get ready to play!")
+
+
+ask_player_name()
