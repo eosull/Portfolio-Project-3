42ed9f6272796a095c8114cf0a0f2a46732eaaa0
eosull <osullivaneoin442@gmail.com>
2023-02-10 11:32:46 +0000

    feat: add class to handle game admin operations

---
+++ admin.py
@@ -0,0 +1,9 @@
+
+class Admin():
+    """
+    Class to perform admin such:
+    * Setting target scores
+    * Checking guesses
+    * Making sure ships are not overlapping
+    * Checking for a win
+    """
