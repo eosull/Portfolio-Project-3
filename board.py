6b5b04e9fc952236399e2255a1be786a213c06ea
eosull <osullivaneoin442@gmail.com>
2023-02-10 10:54:33 +0000

    feat: add class for creation of boards to play game

---
+++ board.py
@@ -0,0 +1,12 @@
+
+class Board():
+    """
+    Class for the creation of boards used for playing Battleship
+    """
+
+    def __init__(self, size, type):
+        """
+        Assigns variables
+        """
+        self.size = size
+        self.type = type
