fbb3373817f2c7708197c6ab83a6f05075de2a6b
eosull <osullivaneoin442@gmail.com>
2023-02-12 08:56:38 +0000

    docs: add docstrings to methods

--- validation.py
+++ validation.py
@@ -2,16 +2,19 @@
 class Validation():
     """
     Class for validation of user input
-    Validates input from the user. Tests carried out dependant on input_type
-    variable passed to function. If input is valid,True is returned and input
-    passes validation. If not, False is returned and input loop continues.
+
+    If input meets conditions,True is returned and input
+    passes validation. If not, False is returned and input
+    loop continues.
     """
 
     def __init__(self, data):
         self.data = data
 
-    # Name must be less than 15 characters & contain only alpha characters
     def validate_name(self):
+        """
+        Name must be less than 15 characters & contain only alpha characters
+        """
         try:
             if len(self.data) > 15:
                 raise ValueError("Name must be less than 15 characters\n")
@@ -21,11 +24,12 @@ class Validation():
         except ValueError as e:
             print(f"Invalid name: {e}Please try again.\n")
             return False
-
         return True
 
-    # Difficulty must be letter 'E', 'M' or 'H'
     def validate_diff(self):
+        """
+        Difficulty must be letter 'E', 'M' or 'H'
+        """
         try:
             if self.data == "E" or self.data == "M" or self.data == "H":
                 return True
@@ -35,8 +39,10 @@ class Validation():
             print(f"Invalid difficulty: {e}Please try again")
             return False
 
-    # Game start input must be letter 'Y' or 'N'
     def validate_start(self):
+        """
+        Game start input must be letter 'Y' or 'N'
+        """
         try:
             if self.data == "Y":
                 print("Ok.... Let's get ready for battle!")
