bc1d0d5ad584c53e44ce32ff95be1c124b3659ef
eosull <osullivaneoin442@gmail.com>
2023-02-12 08:18:59 +0000

    feat: add method and call for name validation

--- run.py
+++ run.py
@@ -10,31 +10,23 @@
 #     """
 #     print("Hello! Welcome to Battleship\n")
 
-#     while True:
-#         name = input("Enter your name:\n")
-
-#         if validate_input("name", name):
-#             print(f"Thank you {name}, let's get ready to play!\n")
-#             break
-
 from admin import Admin
 from ship import Ship
 from board import Board
+from validation import Validation
 
 game = Admin(0, 0, 0, 0, 0, 0)
 
 ship_size = 3
 
+while True:
+    name_input = Validation(input("Enter your name:\n"))
+    if name_input.validate_name():
+        print(f"Thank you {name_input.data}, let's get ready to play!\n")
+        break
 
-def difficulty_choice():
-    """
-    Asks the player to input their difficulty choice. A while loop runs
-    to collect valid string data from the user. This loop runs continuously
-    until valid data is provided. The difficulty value is then returned.
-    This is used to designate grid size.
-    """
+
     print("Please Choose Difficulty")
-
     while True:
         difficulty = input(
             "Press E for Easy, M for Medium and H for Hard:\n").upper()
@@ -95,19 +87,6 @@ def validate_input(input_type, data):
     passes validation. If not, False is returned and input loop continues.
     """
     
-    # Name must be less than 15 characters & contain only alpha characters
-    if input_type == "name":
-        try:
-            if len(data) > 15:
-                raise ValueError("Name must be less than 15 characters\n")
-            for element in data:
-                if element.isalpha() is False:
-                    raise ValueError("Name must only contain letters\n")
-        except ValueError as e:
-            print(f"Invalid name: {e}Please try again.\n")
-            return False
-
-        return True
 
     # Difficulty must be letter 'E', 'M' or 'H'
     if input_type == "difficulty":
--- validation.py
+++ validation.py
@@ -6,3 +6,17 @@ class Validation():
 
     def __init__(self, data):
         self.data = data
+
+    # Name must be less than 15 characters & contain only alpha characters
+    def validate_name(self):
+        try:
+            if len(self.data) > 15:
+                raise ValueError("Name must be less than 15 characters\n")
+            for element in self.data:
+                if element.isalpha() is False:
+                    raise ValueError("Name must only contain letters\n")
+        except ValueError as e:
+            print(f"Invalid name: {e}Please try again.\n")
+            return False
+
+        return True
